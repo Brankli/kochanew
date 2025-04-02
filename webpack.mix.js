74c7afa8d2ead02b1bc24393f8daf5e99724b9ef
brankliz <brankliz7@gmail.com>
2025-04-02 21:32:32 +0300

    kocha first commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,7 +4,7 @@ mix.js("resources/js/app.js", "public/app.js")
     .postCss("resources/css/app.css", "public/css", [
         require("postcss-import"),
         require("tailwindcss"),
-        require("autoprefixer"),
+        require("autoprefixer"),  
     ])
     .js("resources/js/back/back.js", "public/back.js")
     .js("resources/js/login/login.js", "public/login.js")
